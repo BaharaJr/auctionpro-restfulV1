569762a584b9526548f686391babacd11c9eee7a
Bennett <41388684+BaharaJr@users.noreply.github.com>
2020-01-14 16:27:13 +0300

    :sparkles: add declarations and imports for new components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './comps/landing/landing.component';
+import { CategoriesComponent } from './comps/categories/categories.component';
+import { InfoComponent } from './comps/info/info.component';
+import { NavComponent } from './comps/nav/nav.component';
+import { FeedComponent } from './comps/feed/feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    CategoriesComponent,
+    InfoComponent,
+    NavComponent,
+    FeedComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
